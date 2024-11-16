e1138c66dc458be1803974838f0a71762f865228
Rayed <rayed.alam@gmail.com>
2024-11-15 19:07:35 -0500

    Initial Commit

---
+++ Master Brainers/Master_BrainersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Master_BrainersApp.swift
+//  Master Brainers
+//
+//  Created by Rayed Jawad on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Master_BrainersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
